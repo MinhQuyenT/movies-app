a5d8431c1fe1a8f25863085426dc7ce50ebf503c
(SSB-12D9) Quyen.Tran <Niko_No1@outlook.com.vn>
2022-02-08 11:47:35 +0700

    first commit

---
+++ src/app/_core/_services/movies.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+
+  constructor(private http: HttpClient) { }
+
+
+  getMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=4a1b34fb678603a99deaaa174d23b84e');
+  }
+
+}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/_core/_services/movies.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  movies: any = [];
+
+  constructor(private moviesService: MoviesService) { }
+
+  ngOnInit(): void {
+    this.moviesService.getMovies().subscribe((res: any) => {
+      this.movies = res.result;
+      console.log(this.movies);
+    })
+  }
+
+}
